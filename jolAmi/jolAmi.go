c296ff4e2e4594be3ca10ca2b099bfbdabbf047d
jahaninia <jahaninia@gmail.com>
2025-10-08 15:27:37 +0330

    update adtions

--- jolAmi/jolAmi.go
+++ jolAmi/jolAmi.go
@@ -10,11 +10,14 @@ import (
 )
 
 type ChannelInfo struct {
-	callerId  int
+	CallerId  int
 	Caller    string
 	Callee    string
+	SRC       string
+	Extension string
 	LinkedID  string
 	Direction int
+	STS       int
 }
 type jolAmi struct {
 	wg              *sync.WaitGroup
@@ -73,6 +76,8 @@ func eventsProcessor(e map[string]string, j *jolAmi) {
 		j.BridgeEnterHandle(e)
 	// case "BridgeLeave":
 	// 	BridgeLeaveHandle(e, config.UrlReject, config.Token, config.Length, config.Debug)
+	case "NewConnectedLine":
+		j.NewConnectedLineHandle(e)
 	case "Hangup":
 		j.HangupHandle(e)
 	case "Cdr":
@@ -115,18 +120,34 @@ func (j *jolAmi) DialBeginHandle(data map[string]string) {
 	if !ok {
 		return
 	}
-	if loaded, ok := j.callStore.Load(uniqueid); ok && data["DestExten"] != "s" {
+	if loaded, ok := j.callStore.Load(data["Linkedid"]); ok && data["DestExten"] != "s" {
 		call := loaded.(*ChannelInfo)
+		j.DebugCheck("**************Call Info in memory:%+v\n", call)
+
 		j.DebugCheck("Link: %s => DestCallerIDNum:%s, DestExten:%s, DialString:%s \n", call.LinkedID, data["DestCallerIDNum"], data["DestExten"], data["DialString"])
+		if call.Direction == 0 && call.Callee == "NULL" && call.LinkedID != uniqueid {
+			if data["Context"] == "from-queue" {
+				call.Callee = data["Exten"]
+				call.Extension = call.Callee
+			}
+			if data["Context"] == "macro-dial-one" {
+				call.Callee = data["DialString"]
+				call.Extension = call.Callee
+			}
+
+			call.Callee = data["DestExten"]
+			call.Extension = call.Callee
+
+		}
 		// if data["Context"] == "macro-dialout-trunk" {
 		// }else if data["Context"] == "ext-queues" {
 		// 	call.Callee = data["DestExten"]
 		// } else {
 		// 	call.Callee = data["DialString"]
 		// }
-		// j.callStore.Store(data["Uniqueid"], call)
+		j.callStore.Store(call.LinkedID, call)
 
-		var jsonData = j.OutputByte("{\"ID\": \"%s\",\"STS\": \"1\" ,\"SRC\": \"%s\",\"DAKHELI\": \"%s\",\"IN_OUT\": \"%d\"}", data["Linkedid"], call.Caller, call.Callee, call.Direction)
+		var jsonData = j.OutputByte("{\"ID\": \"%s\",\"STS\": \"%d\" ,\"SRC\": \"%s\",\"DAKHELI\": \"%s\",\"IN_OUT\": \"%d\"}", call.LinkedID, call.STS, call.SRC, call.Extension, call.Direction)
 		var headers = map[string]string{"accept": "*/*", "Content-Type": "application/json", "Access-token": j.token}
 		jolRestApi.SendRequestJSON(j.urlCall, jsonData, headers)
 	}
@@ -149,6 +170,58 @@ func (j *jolAmi) DialBeginHandle(data map[string]string) {
 	// 	var headers = map[string]string{"accept": "*/*", "Content-Type": "application/json", "Access-token": token}
 	// 	jolRestApi.SendRequestJSON(urlAddr, jsonData, headers)
 	// }
+
+}
+func (j *jolAmi) NewConnectedLineHandle(data map[string]string) {
+	j.DebugCheck("NewConnectedLineHandle: {{ %s }}\n", data)
+	return
+	uniqueid, ok := GetUniqueID(data)
+	if !ok {
+		return
+	}
+	if loaded, ok := j.callStore.Load(data["Linkedid"]); ok {
+		call := loaded.(*ChannelInfo)
+		/*^@CallerIDName:ms-ziari
+		  ^@CallerIDNum:211
+		  ^@Channel:SIP/211-0000a3b9
+		  ^@ChannelState:0
+		  ^@ChannelStateDesc:Down
+		  ^@ConnectedLineName:+982184373000
+		  ^@ConnectedLineNum:+982184373000
+		  ^@Context:from-internal
+		  ^@Event:NewConnectedLine
+		  ^@Exten:211
+		  ^@Language:pr
+		  ^@Linkedid:1759898774.928073
+		  ^@Priority:1
+		  ^@Privilege:call,all
+		  ^@TimeReceived:2025-10-08T08:16:30.309225948+03:30
+		  ^@Uniqueid:1759898790.928076]
+		*/
+
+		j.DebugCheck("Link: %s => DestCallerIDNum:%s, DestExten:%s, DialString:%s \n", call.LinkedID, data["DestCallerIDNum"], data["DestExten"], data["DialString"])
+		if call.Direction == 0 && call.Callee == "NULL" && call.LinkedID != uniqueid && data["ChannelState"] == "4" {
+			if data["Context"] == "from-queue" {
+				call.Callee = data["Exten"]
+				call.Extension = call.Callee
+			}
+
+			call.Callee = data["DestExten"]
+			call.Extension = call.Callee
+
+		}
+		// if data["Context"] == "macro-dialout-trunk" {
+		// }else if data["Context"] == "ext-queues" {
+		// 	call.Callee = data["DestExten"]
+		// } else {
+		// 	call.Callee = data["DialString"]
+		// }
+		j.callStore.Store(call.LinkedID, call)
+
+		var jsonData = j.OutputByte("{\"ID\": \"%s\",\"STS\": \"%d\" ,\"SRC\": \"%s\",\"DAKHELI\": \"%s\",\"IN_OUT\": \"%d\"}", call.LinkedID, call.STS, call.SRC, call.Extension, call.Direction)
+		var headers = map[string]string{"accept": "*/*", "Content-Type": "application/json", "Access-token": j.token}
+		jolRestApi.SendRequestJSON(j.urlCall, jsonData, headers)
+	}
 
 }
 
@@ -176,18 +249,21 @@ func (j *jolAmi) BridgeEnterHandle(data map[string]string) {
 	j.DebugCheck("BridgeEnterHandle: {{ %s }}\n", data)
 
 	// ***
-	uniqueid, ok := GetUniqueID(data)
+	_, ok := GetUniqueID(data)
 	if !ok {
 		return
 	}
-	if loaded, ok := j.callStore.Load(uniqueid); ok && data["ChannelState"] == "6" {
+	if loaded, ok := j.callStore.Load(data["Linkedid"]); ok && data["ChannelState"] == "6" {
 		call := loaded.(*ChannelInfo)
+		call.STS = 2
 		j.DebugCheck("Link: %s => ConnectedLineNum:%s, CallerIDNum:%s, Exten:%s \n", call.LinkedID, data["ConnectedLineNum"], data["CallerIDNum"], data["Exten"])
+
 		call.Callee = data["ConnectedLineNum"]
+		call.Extension = call.Callee
 
-		j.callStore.Store(data["Uniqueid"], call)
+		j.callStore.Store(call.LinkedID, call)
 
-		var jsonData = j.OutputByte("{\"ID\": \"%s\",\"STS\": \"1\" ,\"SRC\": \"%s\",\"DAKHELI\": \"%s\"}", uniqueid, call.Caller, call.Callee)
+		var jsonData = j.OutputByte("{\"ID\": \"%s\",\"STS\": \"%d\" ,\"SRC\": \"%s\",\"DAKHELI\": \"%s\"}", call.LinkedID, call.STS, call.SRC, call.Extension)
 		var headers = map[string]string{"accept": "*/*", "Content-Type": "application/json", "Access-token": j.token}
 		jolRestApi.SendRequestJSON(j.urlAnswer, jsonData, headers)
 	}
@@ -216,44 +292,68 @@ func (j *jolAmi) BridgeEnterHandle(data map[string]string) {
 }
 func (j *jolAmi) HangupHandle(data map[string]string) {
 	j.DebugCheck("HangupHandle: {{ %s }}\n", data)
-	callerID := len(data["CallerIDNum"])
-	var called, DAKHELI string
-	if (callerID == j.lengthExtension && len(data["ConnectedLineNum"]) > j.lengthExtension || len(data["ConnectedLineNum"]) == j.lengthExtension && callerID > j.lengthExtension) && data["Linkedid"] == data["Uniqueid"] && data["ChannelState"] == "6" {
 
-		if callerID > j.lengthExtension {
-			called = Normalization(data["CallerIDNum"])
-			DAKHELI = data["ConnectedLineNum"]
-			data["CallerIDNum"] = called
-		} else {
-			called = Normalization(data["ConnectedLineNum"])
-			DAKHELI = data["CallerIDNum"]
-			data["ConnectedLineNum"] = called
+	// ***
+	uniqueid, ok := GetUniqueID(data)
+	if !ok {
+		return
+	}
+
+	if loaded, ok := j.callStore.Load(data["Linkedid"]); ok && data["Exten"] == "h" {
+		call := loaded.(*ChannelInfo)
+		if call.Direction == 0 && call.LinkedID == uniqueid {
+			call.Callee = data["ConnectedLineNum"]
+			call.Extension = call.Callee
+
 		}
-		var jsonData = j.OutputByte("{\"uniqueId\": \"%s\",\"callerId\": \"%s\",\"DAKHELI\": \"%s\"}", data["Linkedid"], called, DAKHELI)
+		j.callStore.Store(call.LinkedID, call)
+
+		var jsonData = j.OutputByte("{\"ID\": \"%s\",\"STS\": \"%d\" ,\"SRC\": \"%s\",\"DAKHELI\": \"%s\",\"IN_OUT\": \"%d\"}", call.LinkedID, call.STS, call.SRC, call.Extension, call.Direction)
 		var headers = map[string]string{"accept": "*/*", "Content-Type": "application/json", "Access-token": j.token}
 		jolRestApi.SendRequestJSON(j.urlReject, jsonData, headers)
+
+		// ***
+		/*
+			callerID := len(data["CallerIDNum"])
+			var called, DAKHELI string
+			if (callerID == j.lengthExtension && len(data["ConnectedLineNum"]) > j.lengthExtension || len(data["ConnectedLineNum"]) == j.lengthExtension && callerID > j.lengthExtension) && data["Linkedid"] == data["Uniqueid"] && data["ChannelState"] == "6" {
+
+				if callerID > j.lengthExtension {
+					called = Normalization(data["CallerIDNum"])
+					DAKHELI = data["ConnectedLineNum"]
+					data["CallerIDNum"] = called
+				} else {
+					called = Normalization(data["ConnectedLineNum"])
+					DAKHELI = data["CallerIDNum"]
+					data["ConnectedLineNum"] = called
+				}
+				var jsonData = j.OutputByte("{\"uniqueId\": \"%s\",\"callerId\": \"%s\",\"DAKHELI\": \"%s\"}", data["Linkedid"], called, DAKHELI)*/
+		//var headers = map[string]string{"accept": "*/*", "Content-Type": "application/json", "Access-token": j.token}
+		//jolRestApi.SendRequestJSON(j.urlReject, jsonData, headers)
+
 	}
 }
 
 func (j *jolAmi) EndCallHandle(data map[string]string) {
 	j.DebugCheck("CDR: {{ %s }}\n", data)
-	uniqueId, ok := GetUniqueID(data)
+	_, ok := GetUniqueID(data)
 	if !ok {
 		return
 	}
 	// **
 
-	if loaded, ok := j.callStore.Load(uniqueId); ok {
+	if loaded, ok := j.callStore.Load(data["Linkedid"]); ok {
 		call := loaded.(*ChannelInfo)
-		STS := 3
+
 		duration := "0"
+		call.STS = 3
 		if data["Disposition"] == "ANSWERED" {
-			STS = 2
+			call.STS = 2
 			duration = data["Duration"]
 		}
-		var jsonData = j.OutputByte("{\"ID\": \"%s\",\"STS\":\"%d\",\"SRC\": \"%s\",\"DAKHELI\": \"%s\",\"DELAY_TIME\":\"%s\",\"fileUrl\":\"%s\"}", call.LinkedID, STS, call.Caller, call.Callee, duration, "")
+		var jsonData = j.OutputByte("{\"ID\": \"%s\",\"STS\":\"%d\",\"SRC\": \"%s\",\"DAKHELI\": \"%s\",\"DELAY_TIME\":\"%s\",\"fileUrl\":\"%s\"}", call.LinkedID, call.STS, call.SRC, call.Extension, duration, "")
 		var headers = map[string]string{"accept": "*/*", "Content-Type": "application/json", "Access-token": j.token}
-		j.callStore.Delete(uniqueId)
+		j.callStore.Delete(call.LinkedID)
 		jolRestApi.SendRequestJSON(j.urlEndCall, jsonData, headers)
 	}
 	// **
@@ -294,19 +394,42 @@ func (j *jolAmi) NewchannelHandle(data map[string]string) {
 	if !ok {
 		return
 	}
-	direction := 0
-	if uniqueid == data["Linkedid"] && data["Exten"] != "s" && (len(data["CallerIDNum"]) != len(data["Exten"])) {
-		if data["Context"] == "from-internal" && data["ChannelState"] == "0" {
-			direction = 1
+
+	if loaded, ok := j.callStore.Load(data["Linkedid"]); ok {
+		call := loaded.(*ChannelInfo)
+		if data["Context"] == "from - queue" && call.Direction == 0 {
+			call.Callee = data["Exten"]
+			call.Extension = call.Callee
+		}
+		if data["Context"] == "from-internal" && len(data["CallerIDNum"]) == j.lengthExtension && data["ChannelState"] == "0" {
+			call.Callee = data["CallerIDNum"]
+			call.Extension = call.Callee
 		}
 
-		j.callStore.Store(data["Uniqueid"], &ChannelInfo{
-			callerId:  len(data["CallerIDNum"]),
-			Caller:    data["CallerIDNum"],
-			Callee:    data["Exten"][1:],
-			Direction: direction,
-			LinkedID:  uniqueid,
-		})
+		j.callStore.Store(call.LinkedID, call)
+
+	} else if uniqueid == data["Linkedid"] && data["Exten"] != "s" {
+		var channel ChannelInfo
+		channel.Direction = 0
+		channel.LinkedID = uniqueid
+		channel.CallerId = len(data["CallerIDNum"])
+		channel.STS = 1
+
+		if channel.CallerId > j.lengthExtension && data["Context"] == "from-trunk" {
+			channel.Caller = Normalization(data["CallerIDNum"])
+			channel.SRC = channel.Caller
+			channel.Callee = "NULL"
+		}
+
+		if data["Context"] == "from-internal" && data["ChannelState"] == "0" {
+			channel.Direction = 1
+			channel.Callee = data["Exten"][1:]
+			channel.Caller = data["CallerIDNum"]
+			channel.Extension = channel.Caller
+			channel.SRC = channel.Callee
+		}
+
+		j.callStore.Store(data["Uniqueid"], &channel)
 	}
 }
 func Normalization(callerID string) (normalizedNumber string) {
